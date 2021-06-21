34087e2de49325aef007ebbd4b7d8d27dee7c2e2
alanbev <ajbeveridge@tiscali.co.uk>
2021-06-21 12:55:56 +0100

    fix to IDs

---
+++ JS/html._snippets.js
@@ -0,0 +1,8 @@
+pccn_question_html=`<p>
+Has the patient entered anything in the PCCN?
+<br>
+<label for="pccn_done"></label>Yes</label>
+<input type="radio" id="pccn_done" name="pccn"/>
+<label for="pccn_not_done">No</label>
+<input type="radio" id="pccn_done" name="pccn"/>
+</p>`
--- JS/index.js
+++ JS/index.js
@@ -14,28 +14,32 @@ let form_elements=[
 ['Concerns from Last Ward Round', true, false, false],  //carries foward from previous enty
 ['Plan from Last Ward Round', true, false, false],      //carries foward from previous ward round
 ['Vital Signs',true, true, true],                       //info-box display summary, button in control_box to pop up ICG in window, comments
-['investigations',true, true, true],                    //info-box display summary, button in control_box to pop up ICG in window, comments
+['Investigations',true, true, true],                    //info-box display summary, button in control_box to pop up ICG in window, comments
 ['Medications',true, true,true],                        //infobox has medication list. button in control panel to open WPMR, box for comments
 ['VTE', true, true, false],                             //info-box displays whether VTE assesssment up to date, control_box has buttons to pop up VTE assessment in window and to acess EPMA
 ['Fluid balance', true, true, true,],                   //info_box shows summary fluid balance, control_box has prsent drop-down menu, vommnets box for additional comments
-['mobilisation', false, false, true],                   //comments on mobilisation
+['Mobilisation', false, false, true],                   //comments on mobilisation
 ['CPR Status', true, true, true],                       //info_box displays current status, contro_box has button to accept status and button to pop up CPR form
 ['Examination Findings', false, false, true],           //entry for examination findings
 ['Notes', false, false, true],                          //free text notes
 ['Complications', false, true, true],                   //three level pull down menu. first chosses system, second chooses complication from system, third chooses clavien dindo severity
 ['Concerns', false, true, true],                        //two level pull dwon menu fist selects system secomd selects particular concern
 ['Estimated Discharge Date',false,true,true],           //control_box has callender selector, fomments box allows comments
-['Ward Moves To Date',true,false,false]
+['Moves To Date',true,false,false]
 ];
+
 
 
 //create DOM elements
 let main=document.getElementById("main")
 form_elements.forEach((element) => {
+id_array=element[0].split(" ");//create valid id by replacing spaces with _
+id_name=id_array.join("_");
+console.log(id_name)
 let new_element=document.createElement(`p`);
-new_element.id=element[0];
+new_element.id=id_name
 new_element.classList.add("outer_element")
-let new_element_content=`<h2 class="element_title">` + element[0] + `</h2><p class="info_box"></p> <p class="control_box"> </p> <p class="comments"><label for=`+ element +`_input class="comments_label">Comments</label for><br/><input type="text" id=`+ element[0] +`_input class="comments_box" </input></p>`;
+let new_element_content=`<h2 class="element_title">` + element[0] + `</h2><p id=`+ id_name + `_info_box class="info_box"></p> <p id= ` + id_name + `_control_box class="control_box"> </p> <p class="comments"><label for=`+ id_name +`_input_box class="comments_label">Comments</label for><br/><input type="text" id=`+ id_name +`_input class="comments_box" </input></p>`;
 new_element.innerHTML=new_element_content;
 
 //hide unwanted components
@@ -53,3 +57,9 @@ if (!element[3])
     }
 main.append(new_element);  
 })
+
+//PCCN review
+pccn_contol_box=document.getElementById('PCCN_Reviewed_control_box')
+pccn_question_holder=document.createElement("p")
+pccn_question_holder.innerHTML=pccn_question_html
+pccn_contol_box.append(pccn_question_holder)
